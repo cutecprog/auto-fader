62282abb6376fe57e7725874d24a6519e207008f
unadherent <unadherent@gmail.com>
2022-09-29 16:25:33 -0400

    Functioning code. MVP

--- auto_fader.ino
+++ auto_fader.ino
@@ -1,13 +1,18 @@
+#include <DmxInput.h>
+#include <DmxInput.pio.h>
+#include <DmxOutput.h>
+#include <DmxOutput.pio.h>
+
 #include <Arduino.h>
 #include "DmxInput.h"
 #include <DmxOutput.h>
 
 #define START_CHANNEL 1
-#define NUM_CHANNELS 192
-#define UNIVERSE_LENGTH 193
-#define RGB_AMOUNT 5
+#define NUM_CHANNELS 196
+#define UNIVERSE_LENGTH 197
+#define RGB_AMOUNT 1
 #define RGB_SIZE 4
-#define WHITE_AMOUNT 2
+#define WHITE_AMOUNT 0
 #define USER_ADDRESS 177
 
 DmxInput dmx_input;
@@ -21,7 +26,9 @@ uint8_t white[WHITE_AMOUNT];
 
 void loop()
 {
-  for (int i=0; i<RGB_AMOUNT; i++) {
+  //output_buffer[5] =255;
+  //output_buffer[6] =255;
+  /*for (int i=0; i<RGB_AMOUNT; i++) {
     for (int j=rgb[i]; j<=RGB_SIZE; j++)
       output_buffer[j] = step(output_buffer[j], input_buffer[j], 1);
     output_buffer[rgb[i]+RGB_SIZE+1] = 0; // set strobe to off
@@ -29,7 +36,10 @@ void loop()
   for (int i=0; i<WHITE_AMOUNT; i++) {
     output_buffer[white[i]] = step(output_buffer[white[i]], input_buffer[white[i]], 1);
     output_buffer[white[i]+1] = 0; // set strobe to off
-  }
+  }*/
+  for (int i = 0; i < UNIVERSE_LENGTH; i++) {
+    output_buffer[i] = step(output_buffer[i], input_buffer[i], 1);
+  }  
   // Send out output_buffer on GPIO-pin 1
   dmx_output.write(output_buffer, NUM_CHANNELS);
   while (dmx_output.busy())
@@ -43,32 +53,6 @@ void loop()
 
 void loop1()
 {  
-  /*input_buffer[1] = 255;
-  input_buffer[2] = 128;
-  input_buffer[3] = 64;
-  input_buffer[4] = 32;  
-  delay(3000);
-  input_buffer[1] = 255;
-  input_buffer[2] = 128;
-  input_buffer[3] = 42;
-  input_buffer[4] = 108;
-  delay(8000);*/
-  /*input_buffer[1] = 255;
-  input_buffer[2] = 51;
-  input_buffer[3] = 179;
-  input_buffer[4] = 102;
-  delay(5000);
-  input_buffer[1] = 255;
-  input_buffer[2] = 115;
-  input_buffer[3] = 75;
-  input_buffer[4] = 220;
-  delay(7000);
-  input_buffer[1] = 255;
-  input_buffer[2] = 0;
-  input_buffer[3] = 0;
-  input_buffer[4] = 0;
-  delay(4000);*/
-
   delay(30);
 
   if(millis() > 100+dmx_input.latest_packet_timestamp()) {
@@ -108,16 +92,16 @@ void setup()
   dmx_output.begin(0);
 
   // Set all channels in the output_buffer to the max allowed value (512)
-  for (int i = 1; i < UNIVERSE_LENGTH; i++) {
+  for (int i = 0; i < UNIVERSE_LENGTH; i++) {
     output_buffer[i] = 0;
   }
-  rgb[0] = 1;
-  rgb[1] = 17;
-  rgb[2] = 33;
-  rgb[3] = 49;
-  rgb[4] = 65;
+  rgb[0] = 5;
+  /*rgb[1] = 21;
+  rgb[2] = 37;
+  rgb[3] = 53;
+  rgb[4] = 69;
   white[0] = 81;
-  white[1] = 83;
+  white[1] = 83;*/
 }
 
 void setup1()
@@ -125,6 +109,9 @@ void setup1()
   // Setup our DMX Input to read on GPIO 5, from channel 1 to 3
   dmx_input.begin(5, START_CHANNEL, NUM_CHANNELS);
   dmx_input.read_async(input_buffer);
+  for (int i = 0; i < UNIVERSE_LENGTH; i++) {
+    input_buffer[i] = 0;
+  }
 
   // Setup the onboard LED so that we can blink when we receives packets
   pinMode(LED_BUILTIN, OUTPUT);
