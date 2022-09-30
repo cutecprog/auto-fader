906e0a7a6d0a6bfef44df8cda30e1ba3f269d662
unadherent <unadherent@gmail.com>
2022-09-29 22:31:07 -0400

    ahhhhhhhhhhhhh. It works better technically. No I won't explain

--- auto_fader.ino
+++ auto_fader.ino
@@ -8,11 +8,12 @@
 #include <DmxOutput.h>
 
 #define START_CHANNEL 1
-#define NUM_CHANNELS 196
-#define UNIVERSE_LENGTH 197
+#define NUM_CHANNELS 192
+#define UNIVERSE_LENGTH 193
 #define USER_ADDRESS 177
 #define FADE_TIME 82
 #define MASTER 84
+#define MIN_FRAME 9
 
 DmxInput dmx_input;
 DmxOutput dmx_output;
@@ -20,14 +21,17 @@ DmxOutput dmx_output;
 volatile uint8_t input_buffer[DMXINPUT_BUFFER_SIZE(START_CHANNEL, NUM_CHANNELS)];
 uint8_t output_buffer[UNIVERSE_LENGTH];
 unsigned long frame_time;
+uint8_t fadetime;
 
 void setup() {
-  // Start the DMX Output on GPIO-pin 0
-  dmx_output.begin(0);
-  // Start the DMX Input on GPIO 5 (channel 1 to 192)
   dmx_input.begin(5, START_CHANNEL, NUM_CHANNELS);
   // Read continuously in the background
   dmx_input.read_async(input_buffer);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Start the DMX Output on GPIO-pin 0
+  dmx_output.begin(0);
+  // Start the DMX Input on GPIO 5 (channel 1 to 192)
   // Set all channels in the output_buffer to 0
   for (int i = 0; i < UNIVERSE_LENGTH; i++) {
     output_buffer[i] = 0;
@@ -36,45 +40,33 @@ void setup() {
   for (int i = 0; i < UNIVERSE_LENGTH; i++) {
     input_buffer[i] = 0;
   }
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  uint8_t fadetime = 0;
+}
+
+void setup1()
+{
+    
 }
 
 void loop() {
-  frame_time = millis();
   for (int i = 0; i < UNIVERSE_LENGTH; i++) {
     output_buffer[i] = step(output_buffer[i], input_buffer[i], 1);
   }
+  if (input_buffer[FADE_TIME] != 0)
+    fadetime = input_buffer[FADE_TIME];
   output_buffer[FADE_TIME] = 0;
   output_buffer[MASTER] = 0;
-  dmx_output.write(output_buffer, NUM_CHANNELS);
-  while (not_next_frame())
+  while (frame_time+MIN_FRAME > millis())
     ;
-  while (dmx_output.busy())
-    Serial.println("Warning: Not given enough time to send data!");
-  // delay a millisecond for stability (Not strictly necessary)
-  //delay(1);
 }
 
 void loop1() {
-  delay(30);
-
-  if (millis() > 100 + dmx_input.latest_packet_timestamp()) {
-    Serial.println("no data!");
-    return;
-  }
-  // Print the DMX channels
-  Serial.print("Received packet: ");
-  for (uint i = 0; i < sizeof(input_buffer); i++) {
-    Serial.print(input_buffer[i]);
-    Serial.print(", ");
-  }
-  Serial.println("");
-
-  // Blink the LED to indicate that a packet was received
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(10);
-  digitalWrite(LED_BUILTIN, LOW);
+  frame_time = millis();
+  dmx_output.write(output_buffer, NUM_CHANNELS);
+  while (frame_time+MIN_FRAME > millis())
+    ;  
+  while (dmx_output.busy())
+    Serial.println("Bad Waiting");
 }
 
 uint8_t step(uint8_t a, uint8_t b, uint8_t step_value) {
@@ -89,7 +81,7 @@ uint8_t step(uint8_t a, uint8_t b, uint8_t step_value) {
 
 // Return the next frame time in milliseconds vary based of fadetime slider
 bool not_next_frame() {
-  unsigned long offset = input_buffer[FADE_TIME];
+  unsigned long offset = fadetime;
   if (offset >= 240)
     return true;  // Hold current frame
   if (offset < 16)
